d36c3c963bb2967dff144c7006d60499d0a10184
Eric Yap <ericywl@live.com>
2024-04-27 11:08:55 +0800

    Implement handle connection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,11 +12,26 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
+                println!("Accepted new connection");
+                handle_connection(_stream);
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("Error: {}", e);
             }
         }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    if request.get(0).unwrap().to_lowercase() == "ping" {
+        let response = "PONG\r\n";
+        stream.write_all(response.as_bytes()).unwrap();
     }
 }
