0f3c2fc9189ab511b4318a16977df862f84f6e23
Eric Yap <ericywl@live.com>
2024-04-28 10:45:41 +0800

    Add thread pool

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 use std::{
-    io::{self, BufRead, BufReader, BufWriter, Read, Write},
+    io::{self, Read, Write},
     net::{TcpListener, TcpStream},
 };
+
+use redis_starter_rust::ThreadPool;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let pool = ThreadPool::build(4).unwrap();
 
     for _stream in listener.incoming() {
         match _stream {
             Ok(stream) => {
                 println!("Accepted new connection");
-                handle_connection(stream);
+                pool.execute(|| {
+                    handle_connection(stream)
+                        .unwrap_or_else(|e| eprintln!("Handle connection error: {e}"));
+                })
+                .unwrap_or_else(|e| eprintln!("Pool execute error: {e}"));
             }
             Err(e) => {
                 eprintln!("Tcp stream error: {e}");
@@ -22,13 +29,13 @@ fn main() {
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) -> Result<(), io::Error> {
     let mut buf = [0; 512];
     loop {
-        let read_count = stream.read(&mut buf).unwrap();
+        let read_count = stream.read(&mut buf)?;
         if read_count == 0 {
-            break;
+            return Ok(());
         }
-        stream.write(b"+PONG\r\n").unwrap();
+        stream.write(b"+PONG\r\n")?;
     }
 }
