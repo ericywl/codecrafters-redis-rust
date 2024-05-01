75228df7712c2dfb164e7e5f8e42debafeb870a5
Eric Yap <ericywl@live.com>
2024-05-01 17:36:07 +0800

    Add port support

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
+use clap::Parser;
+
 use redis_starter_rust::redis::Redis;
-use tracing::error;
+use tracing::{error, info};
 use tracing_subscriber;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Port to listen to
+    #[arg(short, long, default_value = "6379")]
+    port: String,
+}
 
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
     tracing_subscriber::fmt::init();
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    info!("Logs from your program will appear here!");
 
-    let redis = match Redis::new("127.0.0.1:6379").await {
+    let addr = format!("127.0.0.1:{}", args.port);
+    info!("Listening to {addr}...");
+
+    let redis = match Redis::new(addr).await {
         Ok(r) => r,
         Err(e) => {
             error!("Initialize redis error: {e}");
